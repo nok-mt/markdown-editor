cb10111cee975ee405c659edf79f64fa71de103d
seraku-matsuoka <93689275+seraku-matsuoka@users.noreply.github.com>
2021-11-09 14:30:53 +0900

    2-5 マークダウンのプレビューを作る

--- src/hooks/use_state_with_storage.ts
+++ src/hooks/use_state_with_storage.ts
@@ -1,14 +1,17 @@
-import { useState } from "react";
+import { useState } from "react"; //reactから関数"useState"を読み込む
 
 export const useStateWithStorage = (
-  init: string,
-  key: string
+  //カスタムフック"useStateWithStorage"を宣言
+  init: string, //第一引数にinit=初期値を設定
+  key: string //第二引数にローカルストレージから呼び出すキーを設定
 ): [string, (s: string) => void] => {
-  const [value, setValue] = useState<string>(localStorage.getItem(key) || init);
+  //引数の型と戻り値の型を設定
+  const [value, setValue] = useState<string>(localStorage.getItem(key) || init); //useStateを使用し、値にvalue、更新する関数にsetValue、valueの初期値にローカルストレージに保存されているアイテム or initを代入
 
   const setValueWithStorage = (nextValue: string): void => {
-    setValue(nextValue);
-    localStorage.setItem(key, nextValue);
+    //引数にnextValuを受け取る関数を作成
+    setValue(nextValue); //nextvalueをvalueに上書き
+    localStorage.setItem(key, nextValue); //ローカルストレージにデータを保存する（key: key, value: nextvalue）
   };
 
   return [value, setValueWithStorage];
